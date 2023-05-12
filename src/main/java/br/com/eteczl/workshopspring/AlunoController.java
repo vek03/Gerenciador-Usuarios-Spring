21b485e4bb26a6f36f302bde99868ea61569f9b5
vek03 <vektromboni@gmail.com>
2023-05-12 15:22:59 -0300

    Spring - JAVA

---
+++ src/main/java/br/com/eteczl/workshopspring/AlunoController.java
@@ -0,0 +1,45 @@
+package br.com.eteczl.workshopspring;
+
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class AlunoController{
+
+    private List<Aluno> listagemAlunos = new ArrayList<>();
+
+    @PostMapping("/cadastro-alunos")
+    public RedirectView cadastroAlunoSalvar(Aluno aluno){
+
+        listagemAlunos.add(aluno);
+
+        return new RedirectView("/alunos");
+    }
+
+    @GetMapping("/cadastro-alunos")
+    public ModelAndView cadastroAluno(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("cadastro");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/alunos")
+    public ModelAndView alunoListagem(){
+        var modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("listagem");
+
+        modelAndView.addObject("listagemAlunos", listagemAlunos);
+
+        return modelAndView;
+    }
+
+}
---
+++ src/main/java/br/com/eteczl/workshopspring/HomeController.java
@@ -0,0 +1,23 @@
+package br.com.eteczl.workshopspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public ModelAndView acessoHome(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+
+        Aluno aluno = new Aluno("Victor Cardoso", "victor@gmail.com");
+
+        modelAndView.addObject("primeiroAluno", aluno);
+
+        return modelAndView;
+    }
+
+}
